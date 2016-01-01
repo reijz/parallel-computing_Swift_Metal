20660151e0d2fefc33f785e4c7d1211c9d4beaeb
Jiheng Zhang <jiheng@ust.hk>
2016-01-01 11:52:43 +0800

    Initial Commit

---
+++ ParallelDP/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ParallelDP
+//
+//  Created by Jiheng Zhang on 1/1/2016.
+//  Copyright © 2016 verse. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
