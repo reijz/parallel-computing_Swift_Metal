946a270a1142de578b34a2faa843324dd3dcaa1f
Jiheng Zhang <jiheng@ust.hk>
2016-01-15 19:51:33 +0800

    No need to have exact buffer mapping.

--- ParallelDP/Shaders.metal
+++ ParallelDP/Shaders.metal
@@ -9,16 +9,14 @@
 #include <metal_stdlib>
 using namespace metal;
 
-kernel void sigmoid(const device float *inVector [[ buffer(0) ]],
-                    device float *outVector [[ buffer(1) ]],
+kernel void sigmoid(device float *outVector [[ buffer(0) ]],
                     uint id [[ thread_position_in_grid ]],
                     uint i [[thread_position_in_threadgroup]],
                     uint w [[threadgroup_position_in_grid]],
                     uint S [[threads_per_threadgroup]]) {
-    // This calculates sigmoid for _one_ position (=id) in a vector per call on the GPU
-    // outVector[id] = 1.0 / (1.0 + inVector[id]);
-    if (id != w*S+i)
-        outVector[id]=1;
+
+    if (id == w*S+i)
+        outVector[id] = id;
     else
-        outVector[id]=0;
+        outVector[id] = 0;
 }
--- ParallelDP/main.swift
+++ ParallelDP/main.swift
@@ -9,6 +9,20 @@
 import Foundation
 import MetalKit
 
+// parameter setting for DP
+
+let K = 8
+let L = 3
+
+let numberOfStates = Int(pow(Float(K), Float(L)))
+let unitSize = sizeof(Float)
+let resultBufferSize = numberOfStates*unitSize
+
+// hardcoded to 512 for now (recommendation: read about threadExecutionWidth)
+let threadExecutionWidth = 512
+let numThreadsPerGroup = MTLSize(width:threadExecutionWidth,height:1,depth:1)
+let numGroups = MTLSize(width:(resultBufferSize+threadExecutionWidth-1)/threadExecutionWidth, height:1, depth:1)
+
 // Initialize Metal
 
 // var devices = MTLCopyAllDevices()
@@ -25,43 +39,20 @@ var commandBuffer: MTLCommandBuffer! = commandQueue.commandBuffer()
 var computeCommandEncoder = commandBuffer.computeCommandEncoder()
 
 
-// set up a compute pipeline with Sigmoid function and add it to encoder
+
+// set up a compute pipeline with iteration and add it to encoder
 let sigmoidProgram = defaultLibrary.newFunctionWithName("sigmoid")
 var computePipelineFilter = try device.newComputePipelineStateWithFunction(sigmoidProgram!)
 computeCommandEncoder.setComputePipelineState(computePipelineFilter)
 
 
-// Prepare input data
-var myvector = [Float](count: 2000, repeatedValue: 0)
-for (index, value) in myvector.enumerate() {
-    myvector[index] = Float(index)
-}
+// Prepare shared memory
+let resourceOption = MTLResourceOptions()
+var outVectorBuffer = device.newBufferWithLength(resultBufferSize, options: resourceOption)
+computeCommandEncoder.setBuffer(outVectorBuffer, offset: 0, atIndex: 0)
 
 
-// calculate byte length of input data - myvector
-var myvectorByteLength = myvector.count*sizeofValue(myvector[0])
-var resourceOption = MTLResourceOptions()
-
-// b. create a MTLBuffer - input data that the GPU and Metal and produce
-
-var inVectorBuffer = device.newBufferWithBytes(&myvector, length: myvectorByteLength, options: resourceOption)
-
-// c. set the input vector for the Sigmoid() function, e.g. inVector
-//    atIndex: 0 here corresponds to buffer(0) in the Sigmoid function
-computeCommandEncoder.setBuffer(inVectorBuffer, offset: 0, atIndex: 0)
-
-// d. create the output vector for the Sigmoid() function, e.g. outVector
-//    atIndex: 1 here corresponds to buffer(1) in the Sigmoid function
-var resultdata = [Float](count:myvector.count, repeatedValue: 0)
-var outVectorBuffer = device.newBufferWithBytes(&resultdata, length: myvectorByteLength, options: resourceOption)
-computeCommandEncoder.setBuffer(outVectorBuffer, offset: 0, atIndex: 1)
-
-
-// hardcoded to 32 for now (recommendation: read about threadExecutionWidth)
-var threadsPerGroup = MTLSize(width:32,height:1,depth:1)
-var numThreadgroups = MTLSize(width:(myvector.count+31)/32, height:1, depth:1)
-computeCommandEncoder.dispatchThreadgroups(numThreadgroups, threadsPerThreadgroup: threadsPerGroup)
-
+computeCommandEncoder.dispatchThreadgroups(numThreadsPerGroup, threadsPerThreadgroup: numGroups)
 computeCommandEncoder.endEncoding()
 commandBuffer.commit()
 commandBuffer.waitUntilCompleted()
@@ -70,16 +61,13 @@ commandBuffer.waitUntilCompleted()
 
 // a. Get GPU data
 // outVectorBuffer.contents() returns UnsafeMutablePointer roughly equivalent to char* in C
-var data = NSData(bytesNoCopy: outVectorBuffer.contents(),
-    length: myvector.count*sizeof(Float), freeWhenDone: false)
+var data = NSData(bytesNoCopy: outVectorBuffer.contents(), length: resultBufferSize, freeWhenDone: false)
 // b. prepare Swift array large enough to receive data from GPU
-var finalResultArray = [Float](count: myvector.count, repeatedValue: 0)
+var finalResultArray = [Float](count: numberOfStates, repeatedValue: 0)
 
 // c. get data from GPU into Swift array
-data.getBytes(&finalResultArray, length:myvector.count * sizeof(Float))
+data.getBytes(&finalResultArray, length:resultBufferSize)
 
 print(finalResultArray)
-
-// d. YOU'RE ALL SET!
-
+// print(statesResultA)
 
