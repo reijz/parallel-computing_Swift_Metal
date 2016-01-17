b5b1c35ff48f0d34b051f4912b69ef8cabf5c0bf
Jiheng Zhang <jiheng@ust.hk>
2016-01-17 16:28:07 +0800

    Iterate over multiple period, oscillate between buffer[0] and [1], so no need to copy intermediate period result to host

--- ParallelDP/main.swift
+++ ParallelDP/main.swift
@@ -12,13 +12,14 @@ import MetalKit
 // parameter setting for DP
 let K = 8
 let L = 3
+let T = 5
 
 let numberOfStates = Int(pow(Float(K), Float(L)))
 let unitSize = sizeof(Float)
 let resultBufferSize = numberOfStates*unitSize
 
 // hardcoded to 512 for now (recommendation: read about threadExecutionWidth)
-let threadExecutionWidth = 512
+let threadExecutionWidth = 128
 let numThreadsPerGroup = MTLSize(width:threadExecutionWidth,height:1,depth:1)
 let numGroups = MTLSize(width:(resultBufferSize+threadExecutionWidth-1)/threadExecutionWidth, height:1, depth:1)
 
@@ -29,39 +30,49 @@ var commandQueue: MTLCommandQueue! = device.newCommandQueue()
 
 // Allocate memory on device
 let resourceOption = MTLResourceOptions()
-var bufferA = device.newBufferWithLength(resultBufferSize, options: resourceOption)
-var bufferB = device.newBufferWithLength(resultBufferSize, options: resourceOption)
+var buffer:[MTLBuffer] = [
+    device.newBufferWithLength(resultBufferSize, options: resourceOption),
+    device.newBufferWithLength(resultBufferSize, options: resourceOption)]
+
 
 // Get functions from Shaders and add to MTL library
 var defaultLibrary: MTLLibrary! = device.newDefaultLibrary()
 let initDP = defaultLibrary.newFunctionWithName("initialize")
 let iterateDP = defaultLibrary.newFunctionWithName("iterate")
 
+// Initialize
 var commandBufferInitDP: MTLCommandBuffer! = commandQueue.commandBuffer()
 var encoderInitDP = commandBufferInitDP.computeCommandEncoder()
 var pipelineFilterInit = try device.newComputePipelineStateWithFunction(initDP!)
 encoderInitDP.setComputePipelineState(pipelineFilterInit)
-encoderInitDP.setBuffer(bufferA, offset: 0, atIndex: 0)
+encoderInitDP.setBuffer(buffer[0], offset: 0, atIndex: 0)
 encoderInitDP.dispatchThreadgroups(numThreadsPerGroup, threadsPerThreadgroup: numGroups)
 encoderInitDP.endEncoding()
 commandBufferInitDP.commit()
 commandBufferInitDP.waitUntilCompleted()
 
+// Iterate T periods
+// It's import that t starts from 0%2=0, since we start with buffer[0]
+for t in 0..<T {
+    
+    var commandBufferIterateDP: MTLCommandBuffer! = commandQueue.commandBuffer()
+    var encoderIterateDP = commandBufferIterateDP.computeCommandEncoder()
+    var pipelineFilterIterate = try device.newComputePipelineStateWithFunction(iterateDP!)
+    encoderIterateDP.setComputePipelineState(pipelineFilterIterate)
 
-var commandBufferIterateDP: MTLCommandBuffer! = commandQueue.commandBuffer()
-var encoderIterateDP = commandBufferIterateDP.computeCommandEncoder()
-var pipelineFilterIterate = try device.newComputePipelineStateWithFunction(iterateDP!)
-encoderIterateDP.setComputePipelineState(pipelineFilterIterate)
-encoderIterateDP.setBuffer(bufferA, offset: 0, atIndex: 0)
-encoderIterateDP.setBuffer(bufferB, offset: 0, atIndex: 1)
-encoderIterateDP.dispatchThreadgroups(numThreadsPerGroup, threadsPerThreadgroup: numGroups)
-encoderIterateDP.endEncoding()
-commandBufferIterateDP.commit()
-commandBufferIterateDP.waitUntilCompleted()
+    
+    encoderIterateDP.setBuffer(buffer[t%2], offset: 0, atIndex: 0)
+    encoderIterateDP.setBuffer(buffer[(t+1)%2], offset: 0, atIndex: 1)
+    
+    encoderIterateDP.dispatchThreadgroups(numThreadsPerGroup, threadsPerThreadgroup: numGroups)
+    encoderIterateDP.endEncoding()
+    commandBufferIterateDP.commit()
+    commandBufferIterateDP.waitUntilCompleted()
 
+}
 
 // a. Get GPU data
-var data = NSData(bytesNoCopy: bufferB.contents(), length: resultBufferSize, freeWhenDone: false)
+var data = NSData(bytesNoCopy: buffer[T%2].contents(), length: resultBufferSize, freeWhenDone: false)
 // b. prepare Swift array large enough to receive data from GPU
 var finalResultArray = [Float](count: numberOfStates, repeatedValue: 0)
 // c. get data from GPU into Swift array
